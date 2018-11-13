95b15eb88f05debe709684c7c94a57d0c99ec514
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-13 20:44:07 +0100

    Create Sheaf.rs

---
+++ src/Sheaf.rs
@@ -0,0 +1,26 @@
+pub struct Sheaf {
+
+
+}
+
+impl Sheaf {
+
+  fn addCell() ->  () {
+  
+  }
+  
+  fn getCell() -> Cell {
+  
+  
+  }
+  
+  fn extendCellRecursiveTraverse() -> {
+  
+  
+  }
+  
+  fn defaultSheafOptimizer() -> {
+  
+  
+  }
+}
