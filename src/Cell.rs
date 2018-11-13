ff3883ffc2cbf01e823a5a1153de8efdd028974a
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-13 20:41:24 +0100

    Create Cell.rs

---
+++ src/Cell.rs
@@ -0,0 +1,40 @@
+pub struct Cell {
+  dataDimension : ;
+  dataAssignmentPresent : bool = false;
+  extendedAssignments : HashMap;
+  dataTagType : ;
+  optimizationCell : ;
+  extendFromThisCell : ;
+  bounds : ;
+  
+}
+
+impl Cell {
+  fn setDataAssignment(dataAssignment : ) -> {
+    if dataTagType == dataAssignment.valueType {
+    
+    }
+    else {
+      println!("Cell::SetDataAssignment: Error");
+    
+    }
+  }
+  
+  fn setCompareMethod() -> {
+  
+  
+  }
+  
+  fn setSerializeMethod() -> {
+  
+  }
+  
+  fn setDeserializeMethod() -> {
+  
+  
+  }
+}
+
+
+
+
