3994fc4099cafd5ef0e9c8a970f681662734aadc
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-15 19:38:47 +0100

    Create RelSimplicialHomology.rs

---
+++ src/RelSimplicialHomology.rs
@@ -0,0 +1,165 @@
+fn kernel() -> {
+
+
+
+}
+
+
+fn cokernel() -> {
+
+
+
+}
+
+fn toplexify() -> {
+
+
+
+}
+
+fn ksublists() -> {
+
+
+
+}
+
+
+fn simpcluded() -> {
+
+
+
+}
+
+fn ksimplices() -> {
+
+
+
+}
+
+fn kflag() -> {
+
+
+
+}
+
+
+fn flag() -> {
+
+
+
+}
+
+fn vietorisRips() -> {
+
+
+
+}
+
+fn closure() -> {
+
+
+
+}
+
+fn sstar() -> {
+
+
+
+}
+
+fn star() -> {
+
+
+
+}
+
+fn boundary() -> {
+
+
+
+}
+
+fn simplicialHomology() -> {
+
+
+
+}
+
+fn homology() -> {
+
+
+
+}
+
+fn homologyRankOnly() -> {
+
+
+
+}
+
+fn localHomology() -> {
+
+
+
+}
+
+fn localHomologyMultithread() -> {
+
+
+
+}
+
+fn localHomologyMultiproc() -> {
+
+
+
+}
+
+fn cone() -> {
+
+
+
+}
+
+fn integerVertices() -> {
+
+
+
+}
+
+
+fn vertexHoplength() -> {
+
+
+
+}
+
+fn vertexLabels2simplices() -> {
+
+
+
+}
+
+fn iteratedCone() -> {
+
+
+
+}
+
+fn complex2perseus() -> {
+
+
+
+}
+
+fn jaccardIndexFaces(face1 : Face, face2 : Face) -> i32{
+  inter = face1Set.intersection(face2Set);
+  if union == 0 {
+    return 1;
+  }
+  else {
+  
+  }
+    
+
+}
