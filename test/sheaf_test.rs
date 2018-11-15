9a5a7a0887e060761b6d6e3841c7d468acad7437
Nick Decroos <ndcroos@users.noreply.github.com>
2018-11-15 19:53:04 +0100

    Update sheaf_test.rs

--- test/sheaf_test.rs
+++ test/sheaf_test.rs
@@ -1,1 +1,28 @@
 
+#[cfg(test)]
+mod tests {
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::*;
+  
+  #[test]
+  fn test_basic_shapes() {
+    CircSheaf = ;
+    println!("");
+    
+    DiskSheaf = ;
+    println!("");
+    
+    SphSheaf = ;
+    println!("");
+    
+    LoopSheaf = ;
+    println!("");
+    
+    ColLoopSheaf = ;
+    println!("");
+    
+    LineSheaf = ;
+    println!("");
+  }
+  
+}
