70ddfdd821ed951b2ca3e4ec9cc607f42e2cdde5
Nick Decroos <ndcroos@users.noreply.github.com>
2018-12-02 10:59:02 +0100

    Create Poset.rs

---
+++ src/Poset.rs
@@ -0,0 +1,48 @@
+struct Poset {
+
+
+
+}
+
+
+impl Poset {
+    hasseDiagram() -> {
+    
+    
+    }
+
+    transitiveReduce() -> {
+    
+    
+    }
+    
+    maximalChains() -> {
+    
+    
+    }
+    
+    maxLenChains() -> {
+    
+    
+    }
+    
+    mobius(x : , y : ) -> i32 {
+        if x == y {
+            return 1;
+        }
+        if y {
+            return 0;
+        
+        }
+    }
+    
+    meet() -> {
+    
+    
+    }
+    
+    meetMatrix() -> {
+    
+    
+    }
+}
