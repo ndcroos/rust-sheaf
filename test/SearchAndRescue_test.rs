b55581f4de719344d39884d414a4226f68b67b86
Nick Decroos <ndcroos@users.noreply.github.com>
2018-12-09 17:38:50 +0100

    Create SearchAndRescue_test.rs

---
+++ test/SearchAndRescue_test.rs
@@ -0,0 +1,30 @@
+
+
+fn distance(v1 : , v2 : ) -> f32 {
+
+
+
+}
+
+
+
+fn angleMetric(a1 :, a2: ) -> f32 {
+
+
+
+}
+
+
+fn minimumOfbearingAndTime() -> {
+
+
+}
+
+
+// https://en.wikipedia.org/wiki/Bearing_(navigation)
+fn bearing() {
+
+
+
+}
+
